21b68ff1e4b38cf3fa8e90d6b5e3395f8fbdc796
Vinicius <vinicius@iMac-de-Vinicius.local>
2020-06-29 13:00:39 -0300

    Implementação da Lógica do IMC e ajustes no Ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
@@ -14,6 +15,42 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  TextEditingController weightController = TextEditingController();
+  TextEditingController heightController = TextEditingController();
+  String _result = "Informe seus Dados!";
+
+  void _resetFields() {
+    setState(() {
+      weightController.text = "";
+      heightController.text = "";
+      _result = "Informe seus Dados!";
+    });
+  }
+
+  void _calculate() {
+    setState(() {
+      double weight = double.parse(weightController.text.replaceAll(",", "."));
+      double height = double.parse(heightController.text) / 100;
+      double imc = weight / (height * height);
+      print(imc);
+      if (imc <= 18.6) {
+        _result = "Abaixo do Peso!";
+      } else if (imc <= 24.9) {
+        _result = "Peso Ideal!";
+      } else if (imc <= 29.9) {
+        _result = "Levemente Acima do Peso!";
+      } else if (imc <= 34.9) {
+        _result = "Obesidade Grau I!";
+      } else if (imc <= 39.9) {
+        _result = "Obesidade Grau II!";
+      } else {
+        _result = "Obesidade Grau III!";
+      }
+
+      FocusScope.of(context).unfocus();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,7 +63,7 @@ class _HomeState extends State<Home> {
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.refresh),
-            onPressed: () {},
+            onPressed: _resetFields,
           ),
         ],
       ),
@@ -56,6 +93,7 @@ class _HomeState extends State<Home> {
                 color: Colors.green,
                 fontSize: 25,
               ),
+              controller: weightController,
             ),
             Padding(
               padding: EdgeInsets.only(top: 10),
@@ -70,6 +108,7 @@ class _HomeState extends State<Home> {
                   color: Colors.green,
                   fontSize: 25,
                 ),
+                controller: heightController,
               ),
             ),
             Padding(
@@ -77,7 +116,7 @@ class _HomeState extends State<Home> {
               child: Container(
                 height: 50,
                 child: RaisedButton(
-                  onPressed: () {},
+                  onPressed: _calculate,
                   color: Colors.green,
                   child: Text(
                     "Calcular",
@@ -89,6 +128,17 @@ class _HomeState extends State<Home> {
                 ),
               ),
             ),
+            Padding(
+              padding: EdgeInsets.only(top: 20),
+              child: Text(
+                _result,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.green,
+                  fontSize: 20,
+                ),
+              ),
+            )
           ],
         ),
       ),
